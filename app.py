fa64baa268d60c20cde8df3765b451ccbd030ab7
shubham0182 <thakorshubham548@gmail.com>
2026-03-27 17:58:06 +0530

    Auto-sync: file changes

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-
 import streamlit as st 
 
-st.title("Hello World")
-st.write("This is my first Streamlit app!")
+st.title("Data Science Portfolio")
+col1,col2,col3 =st.columns([1,1,1])
+col1.header("Project 1")
+col2.header("Project 2")
+col3.header("Project 3")
 
-st.button("Click me!")
+st.sidebar.header("About Me")
+st.sidebar.button('Introduction',icon_position='right')
+st.sidebar.button('Contact',icon_position='right')
+st.sidebar.button('Resume',icon_position='right')
 
-data = st.text_input("Enter your name")
-age = st.radio("Select your age", options=["0-18", "19-35", "36-50", "51+"])
-if age == "0-18":
-    st.write("You are a child.")
-elif age == "19-35":
-    st.write("You are a young adult.")
-elif age == "36-50":
-    st.write("You are an adult.")
-else:
-    st.write("You are a senior.")
-st.write(f"Hello, {data}, you are {age}!")
-
+st.image("image.png",width=200, caption="Data Science")
